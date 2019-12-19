fcd944e8088c8fb4456e2397183dd5e68abf99fa
Luo <luozpei@163.com>
2019-12-19 12:26:46 +0800

    update README

--- Stock.py
+++ Stock.py
@@ -9,6 +9,7 @@ class Stock(object):
     stock_dir_list = [0]*50
     daily_SD = [0]*50
     expect_RE = [0]*50
+    cost = [0]*50
 
     def __init__(self):
         pass
@@ -25,7 +26,8 @@ class Stock(object):
             raw_data = pd.read_csv('./StockDir/{}'.format(stock_dir), index_col='Date')
             print(raw_data['Adj Close'][-1])
             self.daily_SD[i] = raw_data['Adj Close'].std()
-            self.expect_RE[i] = (raw_data['Adj Close'][-1] - raw_data['Adj Close'][0])/raw_data['Adj Close'][0]
+            self.expect_RE[i] = (raw_data['Adj Close'][-1] - raw_data['Adj Close'][0])*100
+            self.cost[i] = raw_data['Adj Close'][-1]*100
         print(self.expect_RE)
         print(self.daily_SD)
 
