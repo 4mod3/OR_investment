77e8138c340240868cdff205c0f71f816f6e230c
Luo <luozpei@163.com>
2019-12-24 17:24:38 +0800

    pick 10 stock

--- Stock.py
+++ Stock.py
@@ -5,11 +5,17 @@ import pandas as pd
 
 class Stock(object):
 
+    # 初始化变量
     stock_name_list = [0]*50
     stock_dir_list = [0]*50
-    daily_SD = [0]*50
-    expect_RE = [0]*50
-    cost = [0]*50
+    adj_close = pd.DataFrame()
+    adj_close_10 = pd.DataFrame()
+    expect_RE_50 = [0]*50
+    daily_SD_10 = [0] * 10
+    expect_RE_10 = [0] * 10
+    cost_10 = [0] * 10
+    stock_name_10 = [0]*10
+    corr_10 = pd.DataFrame()
 
     def __init__(self):
         pass
@@ -24,13 +30,25 @@ class Stock(object):
 
         for i, stock_dir in enumerate(self.stock_dir_list):
             raw_data = pd.read_csv('./StockDir/{}'.format(stock_dir), index_col='Date')
-            #print(raw_data['Adj Close'][-1])
-            self.daily_SD[i] = raw_data['Adj Close'].std()
-            self.expect_RE[i] = (raw_data['Adj Close'][-1] - raw_data['Adj Close'][0])/*100
-            self.cost[i] = raw_data['Adj Close'][-1]*100
-        print(self.expect_RE)
-        print(self.daily_SD)
+            self.adj_close[i] = raw_data['Adj Close']
+            self.expect_RE_50[i] = (raw_data['Adj Close'][-1] - raw_data['Adj Close'][0])/raw_data['Adj Close'][0]
+
+    def pick_10(self):
+        arr_index = np.argsort(self.expect_RE_50)
+        for i in range(10):
+            self.adj_close_10[i] = self.adj_close[arr_index[-i - 1]]
+            self.daily_SD_10[i] = self.adj_close[arr_index[-i - 1]].std()
+            self.expect_RE_10[i] = self.expect_RE_50[arr_index[-i-1]]
+            self.cost_10[i] = self.adj_close[arr_index[-i - 1]]*100
+            self.stock_name_10[i] = self.stock_name_list[arr_index[-i-1]]
+        self.corr_10 = self.adj_close_10.corr()
 
 
 ss = Stock()
 ss.load()
+ss.pick_10()
+print(ss.adj_close_10)
+print(ss.daily_SD_10)
+print(ss.expect_RE_10)
+print(ss.stock_name_10)
+print(ss.corr_10)
